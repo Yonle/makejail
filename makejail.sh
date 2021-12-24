7a5c321c5369a1fd2a2c1f64662a1a99fb4f1b81
Yonle <yonle@protonmail.com>
2021-12-24 18:24:40 +0700

    Initial Release

---
+++ makejail.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# https://github.com/Yonle/makejail
+
+if [ $# = 0 ]; then
+	printf "Usage: makejail <commands>\nThis will create a jail bootstrap with several commands.\nEach commands is separated by space.\n\nTo make a jail bootstrap in a different filename, Append JAIL_FILENAME variable.\n"
+else
+	paths=""
+	for cmd in $@; do
+		! command -v $cmd > /dev/null && echo "$cmd not found" && exit 1
+		paths="$paths $(command -v $cmd) `ldd $(command -v $cmd) | grep -o "/.*" | cut -d"(" -f1`"
+	done
+
+	echo "Proceed creating tarballs...."
+
+	tar -cvaf $(JAIL_FILENAME:-"JailBootstrap-$(uname)-$(uname -m).tar.gz") $paths
+fi
