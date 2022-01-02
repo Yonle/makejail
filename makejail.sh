952b8bb3924fcbb1527530f75fc74b74f585545a
Yonle <yonle@protonmail.com>
2022-01-02 20:56:34 +0700

    -

--- makejail.sh
+++ makejail.sh
@@ -5,11 +5,11 @@
 if [ $# = 0 ]; then
 	printf "Usage: makejail <commands>\nThis will create a jail bootstrap with several commands.\nEach commands is separated by space.\n\nTo make a jail bootstrap in a different filename, Append JAIL_FILENAME variable.\n"
 else
-	paths=""
+	paths="$(command -v $@)"
 	for cmd in $@; do
 		! command -v $cmd > /dev/null && echo "$cmd not found" && exit 1
 		libs=`ldd $(command -v $cmd) | grep -o "\(\.*\)\/.* \(\.*\)"`
-		paths="$paths $(command -v $cmd) $libs"
+		paths="$paths $libs"
 	done
 
 	echo "Proceed creating tarballs...."
